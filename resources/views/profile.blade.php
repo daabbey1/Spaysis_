84d5d92253d133bb72d6d87e31601aab56fc1119
daabbey1 <dicksonabbey4515@gmail.com>
2020-12-10 22:17:40 +0000

    profile picture updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -86,17 +86,19 @@
             {{-- First row --}}
             <div class="team-row-1">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-6 sub-team">
                         <div class="about-intro">
-                            <span class="head-team"> Our Team </span>
-                            <p>
+                            <div class="team-box">
+                                <span class="head-team"> Our Team </span>
+                            </div>
+                            <p class="team-para1">
                                 Our team is a global collective of urban experts in, architecture, engineering, civil, transportation,
                                 urban planning,
                                 real estate, landscape, communications, urban development and so much more. With experience in Ghana,
                                 Uganda, Nigeria, USA, Zambia,
                                 Kuwait, Egypt, Morocco, Botswana and Germany, these are the people defining the cities of tomorrow.
                             </p>
-                            <p>
+                            <p class="team-para2">
                                 Our people are the key to our overall success as team. We strongly rely on the vast range of skills,
                                 talents and
                                 experience of our people
@@ -107,16 +109,17 @@
                             </p>
                         </div>
                     </div>
+                    {{-- <div class="col-3"></div> --}}
                     <div class="col-6">
                         <div class="team-row">
                             <div class="row">
                                 {{-- <div class="col-6"> --}}
                                     <div class="card col-6" >
                                         <a href="{{ url('/infrastructure') }}" class="card_link">
-                                            <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                                            <img class="card-img-top" src="images/dp/Marful.png" alt="Card image cap">
                                             <span class="card-text">
-                                                Infrastructure Planning & Engineering <br>
-                                                Infrastructure Planning & Engineering <br>
+                                                Dr. -Ing. Alexander Boakye Marful <br>
+                                                Principal Architect <br>
                                                 Infrastructure Planning & Engineering <br>
                                             </span>
                                         </a>
@@ -125,10 +128,10 @@
                                 {{-- <div class="col-6"> --}}
                                     <div class="card col-6">
                                         <a href="{{ url('/infrastructure') }}" class="card_link">
-                                            <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                                            <img class="card-img-top" src="images/dp/Joe.png" alt="Card image cap">
                                             <span class="card-text">
-                                                Infrastructure Planning & Engineering
-                                                Infrastructure Planning & Engineering
+                                                Arc Joe Danquah <br>
+                                                Senior Project Architect <br>
                                                 Infrastructure Planning & Engineering
                                             </span>
                                         </a>
@@ -145,40 +148,128 @@
             <div class="row">
                 <div class="card col-3">
                     <a href="{{ url('/infrastructure') }}" class="card_link">
-                        <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="images/dp/Adaku.png" alt="Card image cap">
                         <span class="card-text">
-                            Infrastructure Planning & Engineering
-                            Infrastructure Planning & Engineering
+                            Dr. -Ing. Ebenezer Adaku <br>
+                            Cost Engineer/Infrastructure <br>
                             Infrastructure Planning & Engineering
                         </span>
                     </a>
                 </div>
                 <div class="card col-3">
                     <a href="{{ url('/infrastructure') }}" class="card_link">
-                        <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="images/dp/Duah.png" alt="Card image cap">
                         <span class="card-text">
-                            Infrastructure Planning & Engineering
-                            Infrastructure Planning & Engineering
+                            Dr Daniel Duah <br>
+                            Senior Project Architect <br>
                             Infrastructure Planning & Engineering
                         </span>
                     </a>
                 </div>
                 <div class="card col-3">
                     <a href="{{ url('/infrastructure') }}" class="card_link">
-                        <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="images/dp/Doh.png" alt="Card image cap">
                         <span class="card-text">
-                            Infrastructure Planning & Engineering
-                            Infrastructure Planning & Engineering
+                            Patrick Doh <br>
+                            Quantity Surveyor <br>
                             Infrastructure Planning & Engineering
                         </span>
                     </a>
                 </div>
                 <div class="card col-3">
                     <a href="{{ url('/infrastructure') }}" class="card_link">
-                        <img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="images/dp/James.png" alt="Card image cap">
                         <span class="card-text">
+                            James Appah Dankyi <br>
+                            Associate Architect <br>
                             Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+            </div>
+
+            {{-- Third Row --}}
+            <div class="row">
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Wahab.png" alt="Card image cap">
+                        <span class="card-text">
+                            Ishmael Wahab <br>
+                            Business Development Manager <br>
                             Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Pauline.png" alt="Card image cap">
+                        <span class="card-text">
+                            Pauline Ashiagbor <br>
+                            Administrative Associate <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Kwame.png" alt="Card image cap">
+                        <span class="card-text">
+                            Kwame Nkrumah <br>
+                            Senior AutoCAD Technician <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Maurice.JPG" alt="Card image cap">
+                        <span class="card-text">
+                            Dipl.-Ing. Maurice Migadde  <br>
+                            Sustainability Project Manager <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+            </div>
+
+            {{-- Fouth --}}
+            <div class="row">
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Osei.png" alt="Card image cap">
+                        <span class="card-text">
+                            Hoffmann Omane Osei <br>
+                            Driver & Office Assistant <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Prince.png" alt="Card image cap">
+                        <span class="card-text">
+                            Prince Ofosuhene Marful <br>
+                            Site Supritendent <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Christabel.png" alt="Card image cap">
+                        <span class="card-text">
+                            Christabel Ofei Kwakye <br>
+                            HR Manager <br>
+                            Infrastructure Planning & Engineering
+                        </span>
+                    </a>
+                </div>
+                <div class="card col-3">
+                    <a href="{{ url('/infrastructure') }}" class="card_link">
+                        <img class="card-img-top" src="images/dp/Gyamfi.png" alt="Card image cap">
+                        <span class="card-text">
+                            Prince Adu-Gyamf <br>
+                            Junior Architect  <br>
                             Infrastructure Planning & Engineering
                         </span>
                     </a>
