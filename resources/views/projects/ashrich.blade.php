181561ba744d92d83df8fb3fedc8eac598981481
daabbey1 <dicksonabbey4515@gmail.com>
2020-12-15 16:11:45 +0000

    project info update

--- resources/views/projects/ashrich.blade.php
+++ resources/views/projects/ashrich.blade.php
@@ -5,9 +5,10 @@
         {{-- Image Slider --}}
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <span style="color: white; font-size:60px;"> <i class="fas fa-chevron-down"></i> </span>
+                {{-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li> --}}
+                {{-- <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> --}}
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -40,7 +41,7 @@
         </div>
 
         {{-- Image Selector --}}
-        <div class="row px-5 bg-light py-2 " style=" ">
+        <div class="row px-5 py-2 " style=" background-color:#000022;">
             <div class="col-2 cards" style="width: 100%;">
                 <span data-target="#carouselExampleIndicators" data-slide-to="0" class="active ">
                     <img class="card-img-top" src="../images/Projects/ashrich1.jpg" alt="Card image cap">
@@ -74,12 +75,45 @@
         </div>
 
         {{-- Info about the project --}}
-        <div class="row">
-            <div class="col-6 text-center">
-                Left Side Info
+        <div class="row left-right-info">
+            <div class="col-7">
+                <div class="discription">
+                   <div class="disc-head">
+                       <span class="disc-main-head"> Ashrich Development Training Center </span> <br>
+                        <span class="disc-sub-head"> Kpone, Tema </span>
+                   </div>
+                   <div class="disc-body">
+                       Ashrich Development training Centre is a first of its Kind IT based development training center that seeks to fill a
+                        great need for
+                        highly trained professional and technical people in various sectors of the economy.This Centre, which will is bilingual
+                        (English & French) when completed will attract students from English and French-speaking countries in West and Central
+                        Africa.
+                   </div>
+                </div>
             </div>
-            <div class="col-6 text-center">
-                Right Side Info
+            <div class="col-5">
+                <div class="pro-info">
+                    <div class="pro-head">
+                        <span class="pro-sub-head"> Project Information </span>
+                    </div>
+                    <div class="pro-body">
+                        <table style="width:100%" >
+                            {{-- <caption>Monthlys</caption> --}}
+                            <tr>
+                                <td> Status: </td>
+                                <td> Design Stage-Complete </td>
+                            </tr>
+                            <tr>
+                                <td> Area: </td>
+                                <td> 10 acres </td>
+                            </tr>
+                            <tr>
+                                <td> Estimated Cost: </td>
+                                <td> USD 125,000,000 </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
