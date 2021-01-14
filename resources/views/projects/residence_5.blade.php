1bdcace1d78342eb1c25ad31ee5cb669c90155c4
daabbey1 <dicksonabbey4515@gmail.com>
2021-01-14 14:39:59 +0000

    project update

--- resources/views/projects/residence_5.blade.php
+++ resources/views/projects/residence_5.blade.php
@@ -9,22 +9,22 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="../images/Projects/addo.jpg" alt="First slide">
+                <img class="d-block" src="../images/Projects/addo.jpg" alt="First slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/Projects/addo2.jpg" alt="Second slide">
+                <img class="d-block" src="../images/Projects/addo2.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/Projects/addo3.jpg" alt="Third slide">
+                <img class="d-block" src="../images/Projects/addo3.jpg" alt="Third slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/Projects/a4.pg" alt="Fourth slide">
+                <img class="d-block" src="../images/Projects/a4.pg" alt="Fourth slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/Projects/a4.pg" alt="Fifth slide">
+                <img class="d-block" src="../images/Projects/a4.pg" alt="Fifth slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/Projects/a6.pg" alt="Sixth slide">
+                <img class="d-block" src="../images/Projects/a6.pg" alt="Sixth slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -42,42 +42,48 @@
     </div>
 
     {{-- Image Selector --}}
-    <div class="row px-5 py-2 " style=" background-color:#000022;">
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="0" class="active ">
-                <img class="card-img-top" src="../images/Projects/addo.jpg" alt="Card image cap">
-            </span>
-        </div>
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="1">
-                <img class="card-img-top" src="../images/Projects/addo2.jpg" alt="image">
-            </span>
-        </div>
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="2">
-                <img class="card-img-top" src="../images/Projects/addo3.jpg" alt="image">
-            </span>
-        </div>
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="3">
-                <img class="card-img-top" src="../images/Projects/aa4.pg" alt="image">
-            </span>
-        </div>
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="4">
-                <img class="card-img-top" src="../images/Projects/a4.png" alt="image">
-            </span>
-        </div>
-        <div class="col-2 cards" style="width: 100%;">
-            <span data-target="#carouselExampleIndicators" data-slide-to="5">
-                <img class="card-img-top" src="../images/Projects/a6.png" alt="image">
-            </span>
-        </div>
+    <div class="  py-2 selection-box" style=" background-color:#000022;">
+        <table>
+            <tr>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
+                        <img class=" img-selection" src="../images/Projects/addo.jpg" alt="Card image cap">
+                    </span>
+                </td>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="1">
+                        <img class=" img-selection" src="../images/Projects/addo2.jpg" alt="Card image cap">
+                    </span>
+                </td>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="2">
+                        <img class=" img-selection" src="../images/Projects/addo3.jpg" alt="Card image cap">
+                    </span>
+                </td>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="3">
+                        <img class=" img-selection" src="../images/Projects/aa4.pg" alt="Card image cap">
+                    </span>
+                </td>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="4">
+                        <img class=" img-selection" src="../images/Projects/doh5.jp" alt="Card image cap">
+                    </span>
+                </td>
+                <td>
+                    <span data-target="#carouselExampleIndicators" data-slide-to="5">
+                        <img class=" img-selection" src="../images/Projects/doh6.jp" alt="Card image cap">
+                    </span>
+                </td>
+            </tr>
+        </table>
+    
+    
     </div>
 
     {{-- Info about the project --}}
     <div class="row left-right-info">
-        <div class="col-7">
+        <div class="col-xs-12 col-lg-7 col-xl-7">
             <div class="discription">
                 <div class="disc-head">
                     <span class="disc-main-head"> </span>
@@ -87,7 +93,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-5">
+        <div class="col-xs-12 col-lg-5 col-xl-5">
             <div class="pro-info">
                 <div class="pro-head">
                     <span class="pro-sub-head"> Project Information </span>
